3ffb4a0c6e8b3d2f042cdc43f89af5f7e031b8ec
Victoria Enebeli <86707012+Iamveektoria@users.noreply.github.com>
2022-06-26 01:50:42 +0100

    made the code better

--- script.js
+++ script.js
@@ -1,22 +1,89 @@
-// initializing username variable
-let userName 
-
-// initializing object variable to store details
+// initializing object variable on a global scope to store details
 let userDetailsDatabase = {}
-
-
- // function to display details of user
- function displayDetails(){
-    alert(`YOUR DETAILS\n\nUsername: ${userDetailsDatabase.UserName}\nPhone Number: ${userDetailsDatabase.phoneNumber}\nEmail: ${userDetailsDatabase.Email}`)
-}
-
-
 
 // function to get details
 function getUserDetails(){
+
+    // this function is to check if the username is valid
+    function validateUserName(userName){
+
+        // if the username is less than 10 or 10, it is valid
+        if (userName.length < 10 && userName.length > 0){
+            return true
+        }
+        // if the username is not less than 10, it is not valid
+        else{
+            return false
+        }
     
-    // prompts the user to enter username
-    userName = prompt("Enter your UserName");
+    }
+
+    // this function checks if the email is valid
+    function validateUserEmail(email){
+        // sets email check criteria
+        const emailCheck = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gi;
+        // checks if its an email
+        emailCheckResult = emailCheck.test(email);
+   
+        // if the email contains @, it is valid
+        if (emailCheckResult == true){
+            return true
+        }
+        // if the email does not contain @, it is not valid
+        else{
+            return false
+        }
+    
+    }
+
+
+    // this function is to check if the phone number is valid
+    function validateUserPhoneNumber(userName){
+   
+        // if the phone number is 11, it is valid
+        if (phoneNumber.length == 11){
+            return true
+        }
+        // if the phone number is less than  or greater than 11, it is not valid
+        else{
+            return false
+        }
+    
+    }
+
+
+    
+    // this function is to validate the password
+    function validatePassword(password){
+      
+        // if the password is less than 6, it is not valid
+        if (password.length < 6){
+            return false
+        }
+        // if the password is 6 or greater than 6, it is valid
+        else{
+            return true
+        }
+    }
+
+
+
+    
+    // this function checks if confirmPassword matches password
+    function validateConfirmPassword(confirmPassword){
+     // if confirmPassword does not match password, it is invalid
+        if (confirmPassword != password){
+            return false
+        }
+        // // if confirmPassword matches password, it is valid
+        else{
+            return true
+        }
+    }
+
+    
+    //initialize username variable and prompts the user to enter username
+    let userName = prompt("Enter your UserName");
 
     // if the user cancel or doesnt enter anything the function ends
     if(userName == null){
@@ -61,7 +128,7 @@ function getUserDetails(){
         email = prompt("Enter a valid email");
     }
 
-    //stores the email in the userDetailsDatabase
+    //stores the email in the userDetailsDatabase object
     userDetailsDatabase["Email"] = email
 
 
@@ -91,103 +158,14 @@ function getUserDetails(){
         confirmPassword = prompt("Confirm password doesnt match password, try again!")
     }
 
+    // alert a message after details have been submitted
     alert("Details submitted successfully")
 
+}
 
-
-
-    // this function is to check if the username is valid
-    function validateUserName(userName){
-        // if the username is null, the funtion ends
-        if (userName == null){
-            return
-        }
-        // if the username is less than 10 or 10, it is valid
-        if (userName.length < 10){
-            return true
-        }
-        // if the username is not less than 10, it is not valid
-        else{
-            return false
-        }
-    
-    }
-
-
-    function validateUserEmail(email){
-        // sets email check criteria
-        const emailCheck = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gi;
-        // checks if its an email
-        emailCheckResult = emailCheck.test(email);
-        // if the email is null, the funtion ends
-        if (email == null){
-            return
-        }
-        // if the email contains @, it is valid
-        if (emailCheckResult == true){
-            return true
-        }
-        // if the email does not contain @, it is not valid
-        else{
-            return false
-        }
-    
-    }
-
-
-    // this function is to check if the phone number is valid
-    function validateUserPhoneNumber(userName){
-        // if the phone number is null, the funtion ends
-        if (phoneNumber == null){
-            return
-        }
-        // if the phone number is 11, it is valid
-        if (phoneNumber.length == 11){
-            return true
-        }
-        // if the phone number is less than  or greater than 11, it is not valid
-        else{
-            return false
-        }
-    
-    }
-
-
-    
-    // this function is to validate the password
-    function validatePassword(password){
-        // if the password is empty, the function ends
-        if (password == null){
-            return
-        }
-        // if the password is less than 6, it is not valid
-        if (password.length < 6){
-            return false
-        }
-        // if the password is 6 or greater than 6, it is valid
-        else{
-            return true
-        }
-    }
-
-
-
-    
-    // this function checks if confirmPassword matches password
-    function validateConfirmPassword(confirmPassword){
-        // if confirmPassword is empty the function ends
-        if (confirmPassword == null){
-            return
-        }// if confirmPassword does not match password, it is invalid
-        if (confirmPassword != password){
-            return false
-        }
-        // // if confirmPassword matches password, it is valid
-        else{
-            return true
-        }
-    }
-
+// function to display details of user
+function displayDetails(){
+    alert(`YOUR DETAILS\n\nUsername: ${userDetailsDatabase.UserName}\nPhone Number: ${userDetailsDatabase.phoneNumber}\nEmail: ${userDetailsDatabase.Email}`)
 }
 
 
